e5bfe6daf74ce1a5f4539076999e2e2bdf602e20
Administrator <9051966664@qq.com>
2018-04-26 11:45:30 +0800

    fragment修改传值类型为fragmentManager

--- androidlibrary/src/main/java/org/wcy/android/utils/RxFragmentUtil.java
+++ androidlibrary/src/main/java/org/wcy/android/utils/RxFragmentUtil.java
@@ -1,6 +1,7 @@
 package org.wcy.android.utils;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
@@ -16,7 +17,11 @@ public class RxFragmentUtil {
      * @param idx
      */
     public static int showTab(int frameId, int idx, int currentTab, List<? extends Fragment> fragments, AppCompatActivity activity) {
-        return showTab(frameId, idx, currentTab, fragments, activity, true);
+        return showTab(frameId, idx, currentTab, fragments, activity.getSupportFragmentManager(), true);
+    }
+
+    public static int showTab(int frameId, int idx, int currentTab, List<? extends Fragment> fragments, FragmentManager fragmentManager) {
+        return showTab(frameId, idx, currentTab, fragments, fragmentManager, true);
     }
 
     /**
@@ -24,16 +29,16 @@ public class RxFragmentUtil {
      *
      * @param idx
      */
-    public static int showTab(int frameId, int idx, int currentTab, List<? extends Fragment> fragments, AppCompatActivity activity, boolean isShoWanimation) {
+    public static int showTab(int frameId, int idx, int currentTab, List<? extends Fragment> fragments, FragmentManager fragmentManager, boolean isShoWanimation) {
         if (idx != currentTab) {
             if (idx == -1) {
-                FragmentTransaction ftshow = activity.getSupportFragmentManager().beginTransaction();
+                FragmentTransaction ftshow = fragmentManager.beginTransaction();
                 ftshow.add(frameId, fragments.get(0));
                 ftshow.commitAllowingStateLoss();
             } else {
                 Fragment cufragment = fragments.get(currentTab);
                 cufragment.onPause(); // 暂停当前tab
-                FragmentTransaction fthide = obtainFragmentTransaction(idx, currentTab, activity, isShoWanimation);
+                FragmentTransaction fthide = obtainFragmentTransaction(idx, currentTab, fragmentManager, isShoWanimation);
                 fthide.hide(cufragment);
                 Fragment fragment = fragments.get(idx);
                 if (fragment.isAdded()) {
@@ -55,12 +60,13 @@ public class RxFragmentUtil {
      * @param index
      * @return
      */
-    public static FragmentTransaction obtainFragmentTransaction(int index, int index2, AppCompatActivity appCompatActivity) {
-        return obtainFragmentTransaction(index, index2, appCompatActivity, true);
+    public static FragmentTransaction obtainFragmentTransaction(int index, int index2, FragmentManager fragmentManager) {
+        return obtainFragmentTransaction(index, index2, fragmentManager, true);
     }
 
-    public static FragmentTransaction obtainFragmentTransaction(int index, int index2, AppCompatActivity appCompatActivity, boolean isShoWanimation) {
-        FragmentTransaction ft = appCompatActivity.getSupportFragmentManager().beginTransaction();
+    public static FragmentTransaction obtainFragmentTransaction(int index, int index2, FragmentManager fragmentManager, boolean isShoWanimation) {
+        // FragmentTransaction ft = appCompatActivity.getSupportFragmentManager().beginTransaction();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
         if (isShoWanimation) {
             //        // 设置切换动画
             if (index > index2) {
