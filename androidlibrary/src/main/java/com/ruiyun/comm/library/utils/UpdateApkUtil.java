b0bac0fed767cbd3969fb87b864adad21103c0bb
wcywangchaoyong <905196664@qq.com>
2019-04-18 04:22:09 +0800

    版本更新处理

--- androidlibrary/src/main/java/com/ruiyun/comm/library/utils/UpdateApkUtil.java
+++ androidlibrary/src/main/java/com/ruiyun/comm/library/utils/UpdateApkUtil.java
@@ -1,12 +1,19 @@
 package com.ruiyun.comm.library.utils;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.fastjson.JSONObject;
+import com.allenliu.versionchecklib.callback.APKDownloadListener;
 import com.allenliu.versionchecklib.v2.AllenVersionChecker;
 import com.allenliu.versionchecklib.v2.builder.DownloadBuilder;
 import com.allenliu.versionchecklib.v2.builder.UIData;
@@ -16,12 +23,14 @@ import com.ruiyun.comm.library.common.JConstant;
 import com.ruiyun.comm.library.mvvm.RxResult;
 import com.ruiyun.comm.library.mvvm.interfaces.CallBack;
 
+import org.wcy.android.BuildConfig;
 import org.wcy.android.R;
 import org.wcy.android.utils.AESOperator;
 import org.wcy.android.utils.RxActivityTool;
 import org.wcy.android.utils.RxLogTool;
 import org.wcy.android.utils.RxTool;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -39,17 +48,17 @@ import okhttp3.Response;
  * @author wcy
  */
 public class UpdateApkUtil {
+    private static String TAG = "UpdateApkUtil";
+
     public static void Update(Context context, CallBack callBack) {
         if (callBack == null) {
             toastTest(context, "正在检查，请稍候...");
         }
         MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        OkHttpClient.Builder builder = new OkHttpClient.Builder().connectTimeout(6, TimeUnit.SECONDS)
-                .writeTimeout(6, TimeUnit.SECONDS).readTimeout(6, TimeUnit.SECONDS);
+        OkHttpClient.Builder builder = new OkHttpClient.Builder().connectTimeout(6, TimeUnit.SECONDS).writeTimeout(6, TimeUnit.SECONDS).readTimeout(6, TimeUnit.SECONDS);
         OkHttpClient okHttpClient = builder.build();
         RequestBody body = RequestBody.create(JSON, "");
-        final Request request = new Request.Builder().url(JConstant.getHttpUrl() + JConstant.VersionName).addHeader("headers", JConstant.getHeardsVal()).post(body)
-                .build();
+        final Request request = new Request.Builder().url(JConstant.getHttpUrl() + JConstant.VersionName).addHeader("headers", JConstant.getHeardsVal()).post(body).build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
@@ -72,6 +81,7 @@ public class UpdateApkUtil {
                                 JSONObject jsonObject = baseResult.getResult();
                                 baseResult.setResult(jsonObject.toJSONString());
                             }
+                            RxLogTool.d("onResponse", baseResult.getResult());
                             VersionBean uploadBean = JSONObject.parseObject(baseResult.getResult(), VersionBean.class);
                             baseResult.setResult(uploadBean);
                             //判断服务器是否有新版本
@@ -80,12 +90,12 @@ public class UpdateApkUtil {
                                 uiData.setDownloadUrl(uploadBean.download_url);
                                 uiData.setTitle(uploadBean.title);
                                 uiData.setContent(uploadBean.getUpdateContent());
-                                DownloadBuilder builder = AllenVersionChecker
-                                        .getInstance()
-                                        .downloadOnly(uiData);
+                                DownloadBuilder builder = AllenVersionChecker.getInstance().downloadOnly(uiData);
                                 builder.setForceRedownload(true);
+
                                 builder.setCustomVersionDialogListener((context, versionBundle) -> {
                                     BaseDialog baseDialog = new BaseDialog(context, R.style.BaseDialog, R.layout.update_version_layout);
+                                    TextView versionchecklib_version_dialog_cancel=baseDialog.findViewById(R.id.versionchecklib_version_dialog_cancel);
                                     //versionBundle 就是UIData，之前开发者传入的，在这里可以拿出UI数据并展示
                                     android.view.View viewline = baseDialog.findViewById(R.id.view_line);
                                     TextView tvTitle = baseDialog.findViewById(R.id.tv_title);
@@ -100,19 +110,16 @@ public class UpdateApkUtil {
                                     tv_updatecreate.setText("更新时间：" + uploadBean.createDate);
                                     textView.setText(uploadBean.getUpdateContent());
                                     if (uploadBean.is_must_update) {
+                                        baseDialog.setCancelable(false);
                                         viewline.setVisibility(android.view.View.GONE);
                                         tv_ca.setVisibility(android.view.View.GONE);
+                                        versionchecklib_version_dialog_cancel.setVisibility(View.GONE);
                                     }
                                     return baseDialog;
                                 });
                                 //通过是否有回调函数来判断是否是启动页请求的接口
                                 if (callBack != null) {
-                                    //判断是否是强制更新，如果是强制更新取消下载则直接退出应用
-                                    if (uploadBean.is_must_update) {
-                                        builder.setForceUpdateListener(() -> RxActivityTool.AppExit(RxTool.getContext()));
-                                    } else {//如果不是强制更新取消下载则回调成功函数
-                                        builder.setOnCancelListener(() -> callBack.onNext(baseResult));
-                                    }
+                                    builder.setOnCancelListener(() -> callBack.onNext(baseResult));
                                 }
                                 builder.executeMission(RxTool.getContext());
                             } else {
